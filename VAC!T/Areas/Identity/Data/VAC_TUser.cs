11382a92db96c8a886a6dc1ad5d3e3dfa2493ad1
Kevin Slieker <kevin.slieker.10@gmail.com>
2023-02-02 15:45:34 +0100

    DataBase

---
+++ VAC!T/Areas/Identity/Data/VAC_TUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace VAC_T.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the VAC_TUser class
+public class VAC_TUser : IdentityUser
+{
+    [PersonalData]
+    public string? Name { get; set; }
+
+    [PersonalData]
+    public string? Adress { get; set; }
+
+    [PersonalData]
+    public string? ProfilePicture { get; set; }
+
+    [PersonalData]
+    public string? Motivation { get; set; }
+
+    [PersonalData]
+    public string? CV { get; set; }
+
+}
+
