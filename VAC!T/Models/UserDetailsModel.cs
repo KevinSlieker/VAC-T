2b4d628913d2fc2ab108e1ae1199acd677c53a6d
Kevin Slieker <kevin.slieker.10@gmail.com>
2023-02-14 14:09:13 +0100

    UserDetails

--- VAC!T/Models/UserDetailsModel.cs
+++ VAC!T/Models/UserDetailsModel.cs
@@ -5,28 +5,35 @@ namespace VAC_T.Models
     public class UserDetailsModel
     {
         public string Id { get; set; }
+
+        [Display(Name = "Naam")]
         public string? Name { get; set; }
 
         [Phone]
-        [Display(Name = "Phone number")]
+        [Display(Name = "Telefoonnummer")]
         public string? PhoneNumber { get; set; }
 
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Geboortedatum")]
         public DateTime? BirthDate { get; set; }
 
+        [Display(Name = "Adres")]
         public string? Address { get; set; }
 
         [DataType(DataType.PostalCode)]
+        [Display(Name = "Postcode")]
         public string? Postcode { get; set; }
 
+        [Display(Name = "Woonplaats")]
         public string? Residence { get; set; }
 
         [DataType(DataType.ImageUrl)]
         public string? ProfilePicture { get; set; }
 
+        [Display(Name = "Motivatie")]
         public string? Motivation { get; set; }
 
         [DataType(DataType.Url)]
