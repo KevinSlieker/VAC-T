a2ad9691a84e565695c60ee611e49f5719b5d5de
Kevin Slieker <kevin.slieker.10@gmail.com>
2023-02-20 13:17:13 +0100

    On startup makes admin role and admin user if they don't exist

--- VAC!T/Models/DbInitializer.cs
+++ VAC!T/Models/DbInitializer.cs
@@ -1,7 +1,9 @@
 ﻿using System.Security.Cryptography;
+using System.Xml.Linq;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using VAC_T.Data;
+using VAC_T.Models;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace VAC_T.Models
@@ -14,42 +16,76 @@ namespace VAC_T.Models
                 serviceProvider.GetRequiredService<
                     DbContextOptions<ApplicationDbContext>>()))
             {
-                if (!context.Roles.Any())
+                using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
                 {
-                    context.Roles.AddRange(
-                        new IdentityRole
-                        {
-                            Name = "ROLE_EMPLOYER",
-                            NormalizedName = "ROLE_EMPLOYER",
-                            ConcurrencyStamp = "1"
-                        },
-                        new IdentityRole
-                        {
-                            Name = "ROLE_CANDIDATE",
-                            NormalizedName = "ROLE_CANDIDATE",
-                            ConcurrencyStamp = "1"
-                        }
-                        );
-                    context.SaveChanges();
-                }
-                if (!context.Users.Any())
-                {
-                    using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
-                    {
-                        var manager = scope.ServiceProvider.GetRequiredService<UserManager<VAC_TUser>>();
-                        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                    var manager = scope.ServiceProvider.GetRequiredService<UserManager<VAC_TUser>>();
+                    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
+                    if (!context.Roles.Any(i => i.Name == "ROLE_ADMIN"))
+                    {
+                        context.Roles.Add(
+                            new IdentityRole
+                            {
+                                Name = "ROLE_ADMIN",
+                                NormalizedName = "ROLE_ADMIN",
+                                ConcurrencyStamp = "3"
+                            });
+                        context.SaveChanges();
+                    }
+
+                    if(!context.Users.Any(i => i.Name == "Admin"))
+                    {
+
+                        var userAdmin = new VAC_TUser
+                        {
+                            UserName = "Admin@mail.nl",
+                            Email = "Admin@mail.nl",
+                            EmailConfirmed = true,
+                            PhoneNumber = "123456798",
+                            Name = "Admin",
+                            BirthDate = DateTime.Now,
+                            Address = "Admin",
+                            Postcode = "Admin",
+                            Residence = "Admin",
+                            ProfilePicture = "assets/img/user/profile.png"
+                        };
+                        var result = await manager.CreateAsync(userAdmin, "Admin123!");
+                        await manager.AddToRoleAsync(userAdmin, "ROLE_ADMIN");
+                        context.SaveChanges();
+                    }
+
+
+                    if (!context.Roles.Any())
+                    {
+                        context.Roles.AddRange(
+                            new IdentityRole
+                            {
+                                Name = "ROLE_EMPLOYER",
+                                NormalizedName = "ROLE_EMPLOYER",
+                                ConcurrencyStamp = "1"
+                            },
+                            new IdentityRole
+                            {
+                                Name = "ROLE_CANDIDATE",
+                                NormalizedName = "ROLE_CANDIDATE",
+                                ConcurrencyStamp = "1"
+                            }
+                            );
+                        context.SaveChanges();
+                    }
+                    if (!context.Users.Any())
+                    {
                         var userEmployer = new VAC_TUser
                         {
                             UserName = "EmployerDSM@mail.nl",
                             Email = "EmployerDSM@mail.nl",
-                            EmailConfirmed= true,
+                            EmailConfirmed = true,
                             PhoneNumber = "123456798",
                             Name = "Test Employer DSM",
-                            BirthDate= DateTime.Now,
+                            BirthDate = DateTime.Now,
                             Address = "12 Address Plaza",
                             Postcode = "6666AA",
-                            Residence= "ErgensHuizen",
+                            Residence = "ErgensHuizen",
                             ProfilePicture = "assets/img/user/profile.png"
                             //Role = context.Roles.Local.SingleOrDefault(x => x.Name == "ROLE_EMPLOYER")
                         };
@@ -94,74 +130,75 @@ namespace VAC_T.Models
                         await manager.AddToRoleAsync(userCandidate, "ROLE_CANDIDATE");
 
                         context.SaveChanges();
+
                     }
-                }
-                if (!context.Company.Any())
-                {
-                    context.Company.AddRange(
-                        new Company
-                        {
-                            Name = "DSM",
-                            Description = "Koninklijke DSM N.V. is een wereldwijd, ‘purpose-led’ bedrijf in Gezondheid," +
-                            " Voeding en Bioscience dat vanuit wetenschappelijke basis de gezondheid van mens, dier en planeet verbetert. " +
-                            "Duurzaamheid is voor ons een verantwoordelijkheid, een kernwaarde en staat centraal in alles wat we doen. " +
-                            "Met onze producten en innovatieve oplossingen willen we het leven van mensen verbeteren. " +
-                            "We richten ons hierbij op een goede gezondheid en gezonde, goed smakende en duurzaam geproduceerde voeding voor iedereen. " +
-                            "Denk hierbij aan vitamines, mineralen, eiwitten, gezonde vetzuren, enzymen en andere gezonde ingrediënten die je terugvindt in voedselproducten" +
-                            " voor mens en dier.",
-                            LogoURL = "assets/img/company/dsm.svg",
-                            WebsiteURL = "https://www.dsm.com/nederland/nl_NL/home.html",
-                            Address = "Poststraat 1",
-                            Postcode = "6135KR",
-                            Residence = "Sittard",
-                            User = context.Users.Where(n => n.Name == "Test Employer DSM").FirstOrDefault()
-                        },
-                        new Company
-                        {
-                            Name = "Educom",
-                            Description = "Educom is een professionele ICT-opleider die mensen bij- of omschoolt  tot softwareontwikkelaar. Wij begeleiden werkzoekende ICT-ers," +
-                            " of mensen die van de ICT hun beroep willen maken, door middel van een gedegen en vooral praktijkgericht traineeship naar een baan. ",
-                            LogoURL = "assets/img/company/educom.png",
-                            WebsiteURL = "https://edu-deta.com/",
-                            Address = "D.U. Stikkerstraat 10",
-                            Postcode = "6842CW",
-                            Residence = "Arnhem",
-                            User = context.Users.Where(n => n.Name == "Test Employer Educom").FirstOrDefault()
-                        });
-                    context.SaveChanges();
-                }
-                if (!context.JobOffer.Any())
-                {
-                    context.JobOffer.AddRange(
-                        new JobOffer
-                        {
-                            Name = "Applicatie Beheerder voor DSM Sittard",
-                            Description = "Description 1",
-                            Company = context.Company.Where(n => n.Name == "DSM").FirstOrDefault(),
-                            Level = "Intro",
-                            Residence = "Sittard",
-                            LogoURL = "assets/img/job_offer/windows.png"
-                        },
-                        new JobOffer
-                        {
-                            Name = "Applicatie Beheerder voor DSM Sittard",
-                            Description = "Description 2",
-                            Company = context.Company.Where(n => n.Name == "DSM").FirstOrDefault(),
-                            Level = "Junior",
-                            Residence = "Sittard",
-                            LogoURL = "assets/img/job_offer/windows.png"
-                        },
-                        new JobOffer
-                        {
-                            Name = "C# developer voor Educom Arnhem",
-                            Description = "Description 3",
-                            Company = context.Company.Where(n => n.Name == "Educom").FirstOrDefault(),
-                            Level = "Midior",
-                            Residence = "Arnhem",
-                            LogoURL = "assets/img/job_offer/csharp.png"
-                        }
-                        );
-                    context.SaveChanges();
+                    if (!context.Company.Any())
+                    {
+                        context.Company.AddRange(
+                            new Company
+                            {
+                                Name = "DSM",
+                                Description = "Koninklijke DSM N.V. is een wereldwijd, ‘purpose-led’ bedrijf in Gezondheid," +
+                                " Voeding en Bioscience dat vanuit wetenschappelijke basis de gezondheid van mens, dier en planeet verbetert. " +
+                                "Duurzaamheid is voor ons een verantwoordelijkheid, een kernwaarde en staat centraal in alles wat we doen. " +
+                                "Met onze producten en innovatieve oplossingen willen we het leven van mensen verbeteren. " +
+                                "We richten ons hierbij op een goede gezondheid en gezonde, goed smakende en duurzaam geproduceerde voeding voor iedereen. " +
+                                "Denk hierbij aan vitamines, mineralen, eiwitten, gezonde vetzuren, enzymen en andere gezonde ingrediënten die je terugvindt in voedselproducten" +
+                                " voor mens en dier.",
+                                LogoURL = "assets/img/company/dsm.svg",
+                                WebsiteURL = "https://www.dsm.com/nederland/nl_NL/home.html",
+                                Address = "Poststraat 1",
+                                Postcode = "6135KR",
+                                Residence = "Sittard",
+                                User = context.Users.Where(n => n.Name == "Test Employer DSM").FirstOrDefault()
+                            },
+                            new Company
+                            {
+                                Name = "Educom",
+                                Description = "Educom is een professionele ICT-opleider die mensen bij- of omschoolt  tot softwareontwikkelaar. Wij begeleiden werkzoekende ICT-ers," +
+                                " of mensen die van de ICT hun beroep willen maken, door middel van een gedegen en vooral praktijkgericht traineeship naar een baan. ",
+                                LogoURL = "assets/img/company/educom.png",
+                                WebsiteURL = "https://edu-deta.com/",
+                                Address = "D.U. Stikkerstraat 10",
+                                Postcode = "6842CW",
+                                Residence = "Arnhem",
+                                User = context.Users.Where(n => n.Name == "Test Employer Educom").FirstOrDefault()
+                            });
+                        context.SaveChanges();
+                    }
+                    if (!context.JobOffer.Any())
+                    {
+                        context.JobOffer.AddRange(
+                            new JobOffer
+                            {
+                                Name = "Applicatie Beheerder voor DSM Sittard",
+                                Description = "Description 1",
+                                Company = context.Company.Where(n => n.Name == "DSM").FirstOrDefault(),
+                                Level = "Intro",
+                                Residence = "Sittard",
+                                LogoURL = "assets/img/job_offer/windows.png"
+                            },
+                            new JobOffer
+                            {
+                                Name = "Applicatie Beheerder voor DSM Sittard",
+                                Description = "Description 2",
+                                Company = context.Company.Where(n => n.Name == "DSM").FirstOrDefault(),
+                                Level = "Junior",
+                                Residence = "Sittard",
+                                LogoURL = "assets/img/job_offer/windows.png"
+                            },
+                            new JobOffer
+                            {
+                                Name = "C# developer voor Educom Arnhem",
+                                Description = "Description 3",
+                                Company = context.Company.Where(n => n.Name == "Educom").FirstOrDefault(),
+                                Level = "Midior",
+                                Residence = "Arnhem",
+                                LogoURL = "assets/img/job_offer/csharp.png"
+                            }
+                            );
+                        context.SaveChanges();
+                    }
                 }
             }
         }
