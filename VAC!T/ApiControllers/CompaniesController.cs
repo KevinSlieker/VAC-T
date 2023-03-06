19a4327d660126818a39a5cb1a0b4fede9d87361
Kevin Slieker <kevin.slieker.10@gmail.com>
2023-03-06 13:42:39 +0100

    ApiControllers Companies and JobOffers

---
+++ VAC!T/ApiControllers/CompaniesController.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VAC_T.Data;
+using VAC_T.Models;
+
+namespace VAC_T.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private UserManager<VAC_TUser> _userManager;
+
+        public CompaniesController(ApplicationDbContext context, UserManager<VAC_TUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Companies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Company>>> GetAllCompaniesAsync()
+        {
+            if (_context.Company != null)
+            {
+                NotFound("Database not connected");
+            }
+
+            return await _context.Company.ToListAsync();
+        }
+
+        // GET: api/Companies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Company>> GetCompanyByIdAsync(int? id)
+        {
+            if (id == null || _context.Company == null)
+            {
+                return NotFound();
+            }
+            // Include(c => c.JobOffers) gives an error. It sais it's a loop and postman can process it.
+            var company = await _context.Company
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return company;
+        }
+
+        //public async Task<IActionResult> DetailsForEmployer()
+        //{
+        //    var user = await _userManager.GetUserAsync(User);
+
+        //    if (_context.Company == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var id = _context.Company.Include(x => x.User).Where(x => x.User == user).FirstOrDefault().Id;
+        //    if (id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var company = await _context.Company.Include(c => c.JobOffers)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View("Details", company);
+        //}
+
+        // GET: Companies/Create
+        //public IActionResult Create()
+        //{
+        //    var company = new Company();
+        //    company.LogoURL = "assets/img/company/default.png";
+        //    return View(company);
+        //}
+
+        // POST: Companies/Create
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync([FromBody] Company company)
+        {
+            if (_context.Company == null)
+            {
+                return NotFound("Database not connected");
+            }
+
+            _context.Add(company);
+            await _context.SaveChangesAsync();
+            var userCompany = new VAC_TUser
+            {
+                UserName = "Employer" + company.Name.Replace(" ", "") + "@mail.nl",
+                Email = "Employer" + company.Name.Replace(" ", "") + "@mail.nl",
+                EmailConfirmed = true,
+                PhoneNumber = "123456798",
+                Name = "Employer" + company.Name.Replace(" ", ""),
+                BirthDate = DateTime.Now,
+                ProfilePicture = "assets/img/user/profile.png"
+            };
+            var result = await _userManager.CreateAsync(userCompany, "Employer" + company.Name.Replace(" ", "") + "123!");
+            await _userManager.AddToRoleAsync(userCompany, "ROLE_EMPLOYER");
+            _context.SaveChanges();
+
+            company.User = userCompany;
+            _context.SaveChanges();
+            company = _context.Company.Where(c => c.Name == company.Name).FirstOrDefault();
+            int id = company.Id;
+            return CreatedAtAction(nameof(GetCompanyByIdAsync), new { id }, null);
+        }
+
+        // GET: Companies/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null || _context.Company == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var company = await _context.Company.FindAsync(id);
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(company);
+        //}
+
+        // POST: Companies/Edit/5
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAsync(int id, [FromBody] Company company)
+        {
+            if (_context.Company == null)
+            {
+                return NotFound("Database not connected");
+            }
+
+            if (id != company.Id)
+            {
+                ModelState.AddModelError("Id", "Does not match Id in URL");
+                return BadRequest(ModelState);
+            }
+            _context.Update(company);
+            await _context.SaveChangesAsync();   
+            return NoContent();
+        }
+
+        // GET: Companies/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null || _context.Company == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var company = await _context.Company
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (company == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(company);
+        //}
+
+        // POST: Companies/Delete/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            if (_context.Company == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Company'  is null.");
+            }
+            var company = await _context.Company.Include(c => c.User).FirstOrDefaultAsync(c => c.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            if (company.User == null)
+            {
+                return NotFound();
+            }
+
+            var userId = company.User.Id;
+            var user = await _context.Users.FindAsync(userId);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+            }
+
+            _context.Company.Remove(company);
+
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return (_context.Company?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ VAC!T/ApiControllers/JobOffersController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VAC_T.Data;
+using VAC_T.Models;
+
+namespace VAC_T.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobOffersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<VAC_TUser> _userManager;
+        private readonly SignInManager<VAC_TUser> _signInManager;
+        public static System.Text.Json.Serialization.ReferenceHandler IgnoreCycles { get; }
+
+        public JobOffersController(ApplicationDbContext context, UserManager<VAC_TUser> userManager, SignInManager<VAC_TUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // GET: JobOffers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JobOffer>>> GetAllJobOffersAsync()
+        {
+            if (_context.JobOffer != null)
+            {
+                NotFound("Database not connected");
+            }
+
+            return await _context.JobOffer.ToListAsync();
+            //if (User.IsInRole("ROLE_EMPLOYER") && _signInManager.IsSignedIn(User))
+            //{
+            //    var user = await _userManager.GetUserAsync(User);
+            //    return _context.JobOffer != null ?
+            //                View(await _context.JobOffer.Include(j => j.Company).Where(C => C.Company.User == user).ToListAsync()) :
+            //                Problem("Entity set 'ApplicationDbContext.JobOffer'  is null.");
+            //}
+            //else
+            //{
+            //    return _context.JobOffer != null ?
+            //                View(await _context.JobOffer.Include(j => j.Company).ToListAsync()) :
+            //                Problem("Entity set 'ApplicationDbContext.JobOffer'  is null.");
+            //}
+        }
+
+        // GET: JobOffers/Details/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<JobOffer>> GetJobOfferByIdAsync(int? id)
+        {
+            if (id == null || _context.JobOffer == null)
+            {
+                return NotFound();
+            }
+            //var user = await _userManager.GetUserAsync(User);
+            // .Include(j => j.Solicitations.Where(x => x.User == user))
+            // .Include(j => j.Company.JobOffers)
+            var jobOffer = await _context.JobOffer
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jobOffer == null)
+            {
+                return NotFound();
+            }
+
+            return jobOffer;
+        }
+
+        // GET: JobOffers/Create
+        //public async Task<IActionResult> Create()
+        //{
+        //    var jobOffer = new JobOffer();
+        //    var user = await _userManager.GetUserAsync(User);
+        //    var company = _context.Company.Where(x => x.User == user).First();
+        //    jobOffer.CompanyId = company.Id;
+        //    jobOffer.Residence = company.Residence;
+        //    return View(jobOffer);
+        //}
+
+        // POST: JobOffers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync([FromBody] JobOffer jobOffer)
+        {
+            if (_context.JobOffer == null)
+            { 
+                return NotFound();
+            }
+            ModelState.Remove("Company");
+            jobOffer.Company = await _context.Company.FindAsync(jobOffer.CompanyId);
+            _context.Add(jobOffer);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetJobOfferByIdAsync), new {id = jobOffer.Id}, null);
+        }
+
+        // GET: JobOffers/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null || _context.JobOffer == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var jobOffer = await _context.JobOffer.Include(j => j.Company).FirstOrDefaultAsync(x => x.Id == id);
+        //    if (jobOffer == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(jobOffer);
+        //}
+
+        // POST: JobOffers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPut("{id}")]
+        public async Task<ActionResult> PutAsync(int id, [FromBody] JobOffer jobOffer)
+        {
+            if (_context.JobOffer == null)
+            { 
+                return NotFound();
+            }
+            if (id != jobOffer.Id)
+            {
+                ModelState.AddModelError("Id", "Does not match Id in URL");
+                return BadRequest(ModelState);
+            }
+            ModelState.Remove("Company");
+            jobOffer.Company = await _context.Company.FindAsync(jobOffer.CompanyId);
+            _context.Update(jobOffer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // GET: JobOffers/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null || _context.JobOffer == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var jobOffer = await _context.JobOffer
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (jobOffer == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(jobOffer);
+        //}
+
+        // POST: JobOffers/Delete/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            if (_context.JobOffer == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.JobOffer'  is null.");
+            }
+            var jobOffer = await _context.JobOffer.FindAsync(id);
+            if (jobOffer == null)
+            { 
+                return NotFound();
+            }
+            _context.JobOffer.Remove(jobOffer);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        private bool JobOfferExists(int id)
+        {
+            return (_context.JobOffer?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
