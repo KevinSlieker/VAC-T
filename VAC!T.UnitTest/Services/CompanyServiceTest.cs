e5447e568fc816a38f5fdad72f65c626bc2267cf
JeroenHeemskerk <j.w.heemskerk@gmail.com>
2023-03-28 20:52:45 +0200

    Added fields for all testentities Added seperate context for testdatabase setup Added appointments and solicitations Added test for lazy loading

--- VAC!T.UnitTest/Services/CompanyServiceTest.cs
+++ VAC!T.UnitTest/Services/CompanyServiceTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VAC_T.Business;
+﻿using VAC_T.Business;
 using VAC_T.UnitTest.TestObjects;
 
 namespace VAC_T.UnitTest.Services
@@ -12,15 +7,24 @@ namespace VAC_T.UnitTest.Services
     {
         private TestDbContext _context;
         private CompanyService _service;
+        private int? testCompanyId;
+        private int? someCompanyId;
 
         [SetUp]
         public async Task Setup()
         {
+            // Setup the database in a different context
+            using (TestDbContext context = new TestDbContext())
+            {
+                await context.SetupDatabase();
+                await context.AddTestUsersAsync();
+                await context.AddTestCompaniesAsync();
+                await context.AddTestSolictations();
+                // save the id's for later use
+                testCompanyId = context.TestCompanyId;
+                someCompanyId = context.SomeOtherCompanyId;
+            }
             _context = new TestDbContext();
-            await _context.SetupDatabase();
-            await _context.AddTestUsersAsync();
-            await _context.AddTestCompaniesAsync();
-
             _service = new CompanyService(_context, _context.UserManager);
         }
 
@@ -32,17 +36,26 @@ namespace VAC_T.UnitTest.Services
         public async Task TestGetCompany()
         {
             // prepare
-            var id = _context.Company.First().Id;
+            int id = testCompanyId!.Value;
 
             // run 
             var company = await _service.GetCompanyAsync(id);
 
             // validate
             Assert.That(company, Is.Not.Null);
+            var companyEntry = _context.Entry(company!);
+
             Assert.That(company.Name, Is.EqualTo("TestCompany"));
+            // Test eager and lazy loaded fields
+            Assert.That(companyEntry.Reference(c => c.User).IsLoaded, Is.True);
+            Assert.That(companyEntry.Collection(c => c.JobOffers!).IsLoaded, Is.True);
+            Assert.That(companyEntry.Collection(c => c.Appointments!).IsLoaded, Is.False);
+
             Assert.That(company.User, Is.Not.Null);
-            Assert.That(company.User.Name, Is.EqualTo("TestCompanyUser"));
-            Assert.That(company.Appointments, Is.Null);
+            Assert.That(company.User.Name, Is.EqualTo("testCompanyUser"));
+
+            Assert.That(company.JobOffers, Is.Not.Empty);                
+            Assert.That(company.Appointments, Is.Null); 
         }
     }
 }
--- VAC!T.UnitTest/TestObjects/TestDbContext.cs
+++ VAC!T.UnitTest/TestObjects/TestDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -14,17 +15,62 @@ namespace VAC_T.UnitTest.TestObjects
     /// </summary>
     internal class TestDbContext : ApplicationDbContext
     {
-        public UserManager<VAC_TUser> UserManager { get;  }
+        public UserManager<VAC_TUser> UserManager { get; }
 
-        public TestDbContext() : 
-            base(new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Vac!t").Options) {
+        // Test entities
+        private VAC_TUser? testAdmin;
+        public string? TestAdminId { get { return testAdmin?.Id; } }
+
+        private VAC_TUser? testCompanyUser;
+        public string? TestCompanyUserId { get { return testCompanyUser?.Id; } }
+
+        private VAC_TUser? testUser;
+        public string? TestUserId { get { return testUser?.Id; } }
+
+        private VAC_TUser? testJustJoinedUser;
+        public string? TestJustJoinedUserId { get { return testJustJoinedUser?.Id; } }
+
+        private VAC_TUser? someOtherCompanyUser;
+        public string? TestSomeOtherCompanyUserId { get { return someOtherCompanyUser?.Id; } }
+
+        private Company? testCompany;
+        public int? TestCompanyId { get { return testCompany?.Id; } }
+
+        private Company? someOtherCompany;
+        public int? SomeOtherCompanyId { get { return someOtherCompany?.Id; } }
+
+        private JobOffer? testCompanyJobOffer1;
+        public int? TestCompanyJobOffer1Id { get { return testCompanyJobOffer1?.Id; } }
+
+        private JobOffer? testCompanyJobOffer2;
+        public int? TestCompanyJobOffer2Id { get { return testCompanyJobOffer2?.Id; } }
+
+        private Appointment? testCompanyAppointmentForAnyJobOffer;
+        public int? TestCompanyAppointmentForAnyJobOfferId { get { return testCompanyAppointmentForAnyJobOffer?.Id; } }
+
+        private Appointment? testCompanyAppointmentForJobOffer1;
+        public int? TestCompanyAppointmentForJobOffer1Id { get { return testCompanyAppointmentForJobOffer1?.Id; } }
+
+        private Appointment? testCompanyAppointmentInPast;
+
+        private Solicitation? testUserSolicitationTestCompanyWithAppointmentForJobOffer1;
+
+        private Solicitation? testUserSolicitationTestCompanyForJobOffer2;
+
+        public TestDbContext() :
+            base(new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "Vac!t")
+                .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole())) // Log to the test console
+                //                .EnableSensitiveDataLogging(true)
+                .Options)
+        {
             UserManager = CreateUserManager();
         }
 
         public async Task SetupDatabase()
         {
             await Database.EnsureCreatedAsync();
-            Roles.AddRange(
+            await Roles.AddRangeAsync(
                     new IdentityRole
                     {
                         Name = "ROLE_EMPLOYER",
@@ -47,79 +93,178 @@ namespace VAC_T.UnitTest.TestObjects
             await SaveChangesAsync();
         }
 
-        public async Task AddTestUsersAsync() {
-            await AddUserWithPasswordAndRoleAsync(new VAC_TUser()
-                {
-                    Name = "TestUser",
-                    UserName = "TestUser",
-                    BirthDate = new DateTime(1980, 2, 28),
-                    Address = "TestLane 44",
-                    Postcode = "9988ZZ",
-                    Motivation = "I'm very motivated",
-                    ProfilePicture = "assets/user/1.jpg",
-                    CV = "assets/user/1.pdf",
-                    Email = "testuser@test.nl",
-                    EmailConfirmed = true,
-                }, 
-                "p@ssWord1", "ROLE_CANDIDATE");
+        public async Task AddTestUsersAsync()
+        {
+            testUser = new VAC_TUser()
+            {
+                Name = "testUser",
+                UserName = "testUser",
+                BirthDate = new DateTime(1980, 2, 28),
+                Address = "TestLane 44",
+                Postcode = "9988ZZ",
+                Motivation = "I'm very motivated",
+                ProfilePicture = "assets/testUser/1.jpg",
+                CV = "assets/testUser/1.pdf",
+                Email = "testuser@test.nl",
+                EmailConfirmed = true,
+            };
+            await AddUserWithPasswordAndRoleAsync(testUser, "p@ssWord1", "ROLE_CANDIDATE");
 
-            await AddUserWithPasswordAndRoleAsync(new VAC_TUser()
-                {
-                    Name = "TestCompanyUser",
-                    UserName = "TestCompanyUser",
-                    Address = "TestStreet 3312",
-                    Postcode = "1234ER",
-                    Email = "info@company.nl",
-                    EmailConfirmed = true,
-                }, 
-                "$3cretCode", "ROLE_EMPLOYER");
+            testCompanyUser = new VAC_TUser()
+            {
+                Name = "testCompanyUser",
+                UserName = "testCompanyUser",
+                Address = "TestStreet 3312",
+                Postcode = "1234ER",
+                Email = "info@testCompany.com",
+                EmailConfirmed = true,
+            };
+            await AddUserWithPasswordAndRoleAsync(testCompanyUser, "$3cretCode", "ROLE_EMPLOYER");
 
-            await AddUserWithPasswordAndRoleAsync(new VAC_TUser()
-                {
-                    Name = "TestAdmin",
-                    UserName = "TestAdmin",
-                    Address = "TestSquare 3",
-                    Postcode = "9742EE",
-                    Email = "admin@test.nl",
-                    EmailConfirmed = true,
-                }, 
-                "t0p$ecretCod3", "ROLE_ADMIN");
+            testAdmin = new VAC_TUser()
+            {
+                Name = "testAdmin",
+                UserName = "testAdmin",
+                Address = "TestSquare 3",
+                Postcode = "9742EE",
+                Email = "admin@test.nl",
+                EmailConfirmed = true,
+            };
+            await AddUserWithPasswordAndRoleAsync(testAdmin, "t0p$ecretCod3", "ROLE_ADMIN");
 
-            await AddUserWithPasswordAndRoleAsync(new VAC_TUser()
-                {
-                    Name = "TestJustJoinedUser",
-                    UserName = "TestJustJoinedUser",
-                    ProfilePicture = "assets/img/user/profile.png",
-                    Email = "testuser@test.nl",
-                    EmailConfirmed = true,
-                },
-                "pas$C0de", "ROLE_CANDIDATE");
+            testJustJoinedUser = new VAC_TUser()
+            {
+                Name = "TestJustJoinedUser",
+                UserName = "TestJustJoinedUser",
+                ProfilePicture = "assets/img/testUser/profile.png",
+                Email = "testuser@test.nl",
+                EmailConfirmed = true,
+            };
+            await AddUserWithPasswordAndRoleAsync(testJustJoinedUser, "pas$C0de", "ROLE_CANDIDATE");
+
+            someOtherCompanyUser = new VAC_TUser()
+            {
+                Name = "someOtherCompanyUser",
+                UserName = "someOtherCompanyUser",
+                Address = "TestAvenue 33",
+                Postcode = "1233AA",
+                Email = "info@someOthercompany.nl",
+                EmailConfirmed = true,
+            };
+            await AddUserWithPasswordAndRoleAsync(someOtherCompanyUser, "s0meComp@ny", "ROLE_EMPLOYER");
+
         }
 
         public async Task AddTestCompaniesAsync()
         {
-            await Company.AddRangeAsync(new Company()
-            {
-                Name= "TestCompany",
+            testCompany = new Company {
+                Name = "TestCompany",
                 Description = "A Test Company",
                 LogoURL = "assets/img/company/testcompany.jpg",
                 WebsiteURL = "http://testCompany.com",
                 Address = "Somewherestreet 11",
                 Postcode = "1122AB",
                 Residence = "TestCity",
-                User = Users.First(u => u.Name == "TestCompanyUser")
-            });
+                User = testCompanyUser
+            };
+            await Company.AddAsync(testCompany);
+
+            someOtherCompany = new Company
+            {
+                Name = "Some Other Company",
+                Description = "We make software",
+                LogoURL = "assets/img/company/testcompany.jpg",
+                WebsiteURL = "http://someOtherCompany.com",
+                Address = "Industrial Zone 2",
+                Postcode = "2345RR",
+                Residence = "TestCity",
+                User = someOtherCompanyUser
+            };
+            await Company.AddAsync(someOtherCompany);
+
+            testCompanyJobOffer1 = new JobOffer
+            {
+                Name = "Test Job Offer",
+                Description = "To do some testing",
+                Company = testCompany,
+                Created = DateTime.Now.AddDays(-10),
+                Level = "Beginner",
+                Residence = "TestCity",
+            };
+            await JobOffer.AddAsync(testCompanyJobOffer1);
+
+            testCompanyJobOffer2 = new JobOffer
+            {
+                Name = "Software Tester",
+                Description = "Make a lot of unit tests",
+                Company = testCompany,
+                Created = DateTime.Now.AddDays(-1),
+                Level = "Medior",
+                Residence = "TestCity",
+            };
+            await JobOffer.AddAsync(testCompanyJobOffer2);
+
+
+            testCompanyAppointmentForAnyJobOffer = new Appointment
+            {
+                Company = testCompany,
+                Date = DateTime.Today.AddDays(2),
+                Time = DateTime.UnixEpoch.AddHours(14),
+                Duration = TimeSpan.FromMinutes(45),
+                IsOnline = false,
+                JobOffer = null
+            };
+            await Appointment.AddAsync(testCompanyAppointmentForAnyJobOffer);
+
+            testCompanyAppointmentForJobOffer1 = new Appointment
+            {
+                Company = testCompany,
+                Date = DateTime.Today.AddDays(2),
+                Time = DateTime.UnixEpoch.AddHours(15).AddMinutes(15),
+                Duration = TimeSpan.FromMinutes(45),
+                IsOnline = false,
+                JobOffer = testCompanyJobOffer1,
+            };
+            await Appointment.AddAsync(testCompanyAppointmentForJobOffer1);
+
+            testCompanyAppointmentInPast = new Appointment
+            {
+                Company = testCompany,                
+                Date = DateTime.Today.AddDays(-1),
+                Time = DateTime.UnixEpoch.AddHours(14),
+                Duration = TimeSpan.FromMinutes(45),
+                IsOnline = false,
+                JobOffer = testCompanyJobOffer2,
+            };
+            await Appointment.AddAsync(testCompanyAppointmentInPast);
+
             await SaveChangesAsync();
         }
-        /*
-                public async Task AddTestSolictations()
-                {
+        public async Task AddTestSolictations()
+        {
 
+            testUserSolicitationTestCompanyWithAppointmentForJobOffer1 = new Solicitation
+            {
+                User = testUser!,
+                JobOffer = testCompanyJobOffer1!,
+                Date = DateTime.Now.AddDays(-8),
+                Selected = true,
+                Appointment = testCompanyAppointmentForJobOffer1
+            };
+            await Solicitation.AddAsync(testUserSolicitationTestCompanyWithAppointmentForJobOffer1);
 
+            testUserSolicitationTestCompanyForJobOffer2 = new Solicitation
+            {
+                User = testUser!,
+                JobOffer = testCompanyJobOffer2!,
+                Date = DateTime.Now.AddDays(-1),
+                Selected = false,
+                Appointment = null
+            };
+            await Solicitation.AddAsync(testUserSolicitationTestCompanyForJobOffer2);
 
-                    await SaveChangesAsync();
-                }
-        */
+            await SaveChangesAsync();
+        }
 
 
 
@@ -133,7 +278,7 @@ namespace VAC_T.UnitTest.TestObjects
         /// <returns>The User Manager</returns>
         private UserManager<VAC_TUser> CreateUserManager()
         {
-            var store = new UserStore<VAC_TUser>(this);            
+            var store = new UserStore<VAC_TUser>(this);
 
             var idOptions = new IdentityOptions();
 
@@ -154,7 +299,7 @@ namespace VAC_T.UnitTest.TestObjects
             idOptions.Lockout.AllowedForNewUsers = true;
 
             var options = Options.Create(idOptions);
-            
+
             var userValidators = new List<IUserValidator<VAC_TUser>>();
             UserValidator<VAC_TUser> validator = new UserValidator<VAC_TUser>();
             userValidators.Add(validator);
@@ -176,9 +321,9 @@ namespace VAC_T.UnitTest.TestObjects
         }
 
         /// <summary>
-        /// Create a user with a specific password
+        /// Create a testUser with a specific password
         /// </summary>
-        /// <param name="user">the user</param>
+        /// <param name="user">the testUser</param>
         /// <param name="password">the password</param>
         /// <param name="roleName">either "ROLE_CANDIDATE", "ROLE_EMPLOYER" or "ROLE_ADMIN"</param>
         public async Task AddUserWithPasswordAndRoleAsync(VAC_TUser user, string password, string roleName)
